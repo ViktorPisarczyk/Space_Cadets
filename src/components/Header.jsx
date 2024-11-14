597202ff6106071956390d2897755ad1c50bf9a8
ViktorPisarczyk <viktor.pisarczyk@gmail.com>
2024-11-14 11:16:24 +0100

    Explore link added to navbar and Contact form finalized

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,168 +11,109 @@ import Jupiter from "../assets/Jupiter.png";
 import Saturn from "../assets/Saturn.png";
 import Uranus from "../assets/Uranus.png";
 import Neptune from "../assets/Neptune.png";
-import PlanetDetails from "./PlanetDetails";
 
 function Header() {
-	const [dropdownOpen, setDropdownOpen] = useState(false);
-	const [menuOpen, setMenuOpen] = useState(false);
-	const location = useLocation();
-	const dropdownRef = useRef(null);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const location = useLocation();
+  const dropdownRef = useRef(null);
 
-	const toggleDropdown = () => {
-		setDropdownOpen(!dropdownOpen);
-	};
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
 
-	const toggleMenu = () => {
-		setMenuOpen(!menuOpen);
-	};
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-	useEffect(() => {
-		setDropdownOpen(false);
-		setMenuOpen(false);
+  useEffect(() => {
+    setDropdownOpen(false);
+    setMenuOpen(false);
 
-		const handleClickOutside = (event) => {
-			if (
-				dropdownRef.current &&
-				!dropdownRef.current.contains(event.target)
-			) {
-				setDropdownOpen(false);
-				setMenuOpen(false);
-			}
-		};
-	}, [location]);
+    const handleClickOutside = (event) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.current.target)
+      ) {
+        setDropdownOpen(false);
+        setMenuOpen(false);
+      }
+    };
 
-	return (
-		<header className="header">
-			<div className="logo">
-				<Link to="/">
-					<img
-						className="logo"
-						src={Logo}
-						alt="Logo"
-					/>
-				</Link>
-			</div>
-			<button
-				className="menu-toggle"
-				onClick={toggleMenu}>
-				☰
-			</button>
-			<nav className={`navbar ${menuOpen ? "open" : ""}`}>
-				<Link
-					to="/"
-					className="nav-link">
-					Home
-				</Link>
-				<div
-					className="dropdown"
-					ref={dropdownRef}>
-					<button
-						onClick={toggleDropdown}
-						className="dropdown-btn">
-						Planets
-						<span className={`arrow ${dropdownOpen ? "open" : ""}`}>
-							›
-						</span>
-					</button>
-					{dropdownOpen && (
-						<div className="dropdown-menu">
-							<Link
-								to="/planets/mercury"
-								className="dropdown-item">
-								Mercury
-								<img
-									className="planet-img"
-									src={Mercury}
-									alt="Mercury"
-								/>
-							</Link>
-							<Link
-								to="planets/venus"
-								className="dropdown-item">
-								Venus
-								<img
-									className="planet-img"
-									src={Venus}
-									alt="Venus"
-								/>
-							</Link>
-							<Link
-								to="planets/earth"
-								className="dropdown-item">
-								Earth
-								<img
-									className="planet-img"
-									src={Earth}
-									alt="Earth"
-								/>
-							</Link>
-							<Link
-								to="planets/mars"
-								className="dropdown-item">
-								Mars
-								<img
-									className="planet-img"
-									src={Mars}
-									alt="Mars"
-								/>
-							</Link>
-							<Link
-								to="planets/jupiter"
-								className="dropdown-item">
-								Jupiter
-								<img
-									className="planet-img"
-									src={Jupiter}
-									alt="Jupiter"
-								/>
-							</Link>
-							<Link
-								to="planets/saturn"
-								className="dropdown-item">
-								Saturn
-								<img
-									className="planet-img"
-									src={Saturn}
-									alt="Saturn"
-								/>
-							</Link>
-							<Link
-								to="planets/uranus"
-								className="dropdown-item">
-								Uranus
-								<img
-									className="planet-img"
-									src={Uranus}
-									alt="Uranus"
-								/>
-							</Link>
-							<Link
-								to="planets/neptune"
-								className="dropdown-item">
-								Neptune
-								<img
-									className="planet-img"
-									src={Neptune}
-									alt="Neptune"
-								/>
-							</Link>
-						</div>
-					)}
-				</div>
-				<Link
-					to="/team"
-					className="nav-link">
-					Team
-				</Link>
-				<Link
-					to="/contact"
-					className="nav-link">
-					Contact
-				</Link>
-			</nav>
-		</header>
-	);
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [location]);
+
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">
+          <img className="logo" src={Logo} alt="Logo" />
+        </Link>
+      </div>
+      <button className="menu-toggle" onClick={toggleMenu}>
+        ☰
+      </button>
+      <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+        <Link to="/" className="nav-link">
+          Home
+        </Link>
+        <Link to="/planets" className="nav-link">
+          Explore
+        </Link>
+        <div className="dropdown" ref={dropdownRef}>
+          <button onClick={toggleDropdown} className="dropdown-btn">
+            Planets
+            <span className={`arrow ${dropdownOpen ? "open" : ""}`}>›</span>
+          </button>
+          {dropdownOpen && (
+            <div className="dropdown-menu">
+              <Link to="/planets/mercury" className="dropdown-item">
+                Mercury
+                <img className="planet-img" src={Mercury} alt="Mercury" />
+              </Link>
+              <Link to="planets/venus" className="dropdown-item">
+                Venus
+                <img className="planet-img" src={Venus} alt="Venus" />
+              </Link>
+              <Link to="planets/earth" className="dropdown-item">
+                Earth
+                <img className="planet-img" src={Earth} alt="Earth" />
+              </Link>
+              <Link to="planets/mars" className="dropdown-item">
+                Mars
+                <img className="planet-img" src={Mars} alt="Mars" />
+              </Link>
+              <Link to="planets/jupiter" className="dropdown-item">
+                Jupiter
+                <img className="planet-img" src={Jupiter} alt="Jupiter" />
+              </Link>
+              <Link to="planets/saturn" className="dropdown-item">
+                Saturn
+                <img className="planet-img" src={Saturn} alt="Saturn" />
+              </Link>
+              <Link to="planets/uranus" className="dropdown-item">
+                Uranus
+                <img className="planet-img" src={Uranus} alt="Uranus" />
+              </Link>
+              <Link to="planets/neptune" className="dropdown-item">
+                Neptune
+                <img className="planet-img" src={Neptune} alt="Neptune" />
+              </Link>
+            </div>
+          )}
+        </div>
+        <Link to="/team" className="nav-link">
+          Team
+        </Link>
+        <Link to="/contact" className="nav-link">
+          Contact
+        </Link>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
